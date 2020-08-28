a2d50fa9063a6580acc42874a11b7185bcec72e7
Nicolò <nico.paganelli91@gmail.com>
2020-08-28 17:53:03 +0200

    dati messi in lista html

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
 <!-- Partiamo dal diagramma della tabella stanze nel db hotel (usate phpmyadmin per vedere le caratteristiche della tabella) e creiamo una classe che rifletta la tabella stanze, poi la usiamo per stampare in pagina i dati.
 I dati anche qui non arrivano da db ma da un array. -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>boolean hotel</title>
+  </head>
+  <body>
+    <h1>boolean hotel</h1>
 
-<?php
-require_once(__DIR__ . '/server.php');
-require_once(__DIR__ . '/Stanza.php');
+    <?php
+    require_once(__DIR__ . '/server.php');
+    require_once(__DIR__ . '/Stanza.php');
 
-foreach ($stanze as $stanza) {
-  $singola_stanza = new Stanza(
-    $stanza['id'],
-    $stanza['room_number'],
-    $stanza['floor'],
-    $stanza['beds'],
-    $stanza['created_at'],
-    $stanza['updated_at']
-  );
-  var_dump($singola_stanza);
-}
-?>
+    foreach ($stanze as $stanza) {
+      $singola_stanza = new Stanza(
+        $stanza['id'],
+        $stanza['room_number'],
+        $stanza['floor'],
+        $stanza['beds'],
+        $stanza['created_at'],
+        $stanza['updated_at']
+      );
+    ?>
+
+    <h3><?php echo 'stanza numero: ' . $stanza['room_number']; ?></h3>
+    <ul>
+      <li><?php echo 'id: ' . $stanza['id']; ?></li>
+      <li><?php echo 'piano: ' . $stanza['floor']; ?></li>
+      <li><?php echo 'letti: ' . $stanza['beds']; ?></li>
+      <li><?php echo 'data creazione: ' . $stanza['created_at']; ?></li>
+      <li><?php echo 'data aggiornamento: ' . $stanza['updated_at']; ?></li>
+    </ul>
+    
+    <?php
+    }
+    ?>
+  </body>
+</html>
